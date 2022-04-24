b39d46fc317fee869b6c8259ba82a430e9c63919
RapiDash <srivatsa.m11@gmail.com>
2022-04-24 15:26:00 +0530

    Added database and database deployment for tests

---
+++ Europa/Database/Tables/DeviceInfo.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[DeviceInfo]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] VARCHAR(300) NOT NULL, 
+    [Description] VARCHAR(MAX) NOT NULL, 
+    [Owner] VARCHAR(100) NULL, 
+    [Weight] DECIMAL(13, 5) NULL
+)
---
+++ Europa/WebApi.Tests/Setup.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.SqlServer.Dac;
+using NUnit.Framework;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace WebApi.Tests
+{
+    [SetUpFixture]
+    public class Setup
+    {
+        [OneTimeSetUp]
+        public void DeployDatabase()
+        {
+            var dacServices = new DacServices("Server=localhost;Database=SpaceExplorationPositionTracker;Trusted_Connection=True;");
+            using (var dacPackage = DacPackage.Load("../../../../Database/bin/Debug/Database.dacpac"))
+            {
+                dacServices.Deploy(dacPackage, "SpaceExplorationPositionTracker", true);
+            }
+        }
+
+        [OneTimeTearDown]
+        public void TearDownDatabase()
+        {
+            using (var sqlConnection = new SqlConnection("Server=localhost;Database=master;Trusted_Connection=True;"))
+            {
+                sqlConnection.Open();
+                var sqlCommand = new SqlCommand("DROP DATABASE SpaceExplorationPositionTracker", sqlConnection);
+                sqlCommand.ExecuteNonQuery();
+                sqlConnection.Close();
+            }
+        }
+    }
+}
