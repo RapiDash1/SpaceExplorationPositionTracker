fbc9f2a376bb1d9135b7a2552189adaafa48a2fd
RapiDash <srivatsa.m11@gmail.com>
2022-04-24 11:03:33 +0530

    Initial accessor implementation

---
+++ Europa/WebApi/DataAccessors/IRegisterDeviceAccessor.cs
@@ -0,0 +1,9 @@
+﻿using WebApi.Models;
+
+namespace WebApi.DataAccessors
+{
+    public interface IRegisterDeviceAccessor
+    {
+        Task RegisterNewDevice(RegisterDevice registerDevice);
+    }
+}
---
+++ Europa/WebApi/DataAccessors/RegisterDeviceAccessor.cs
@@ -0,0 +1,12 @@
+﻿using WebApi.Models;
+
+namespace WebApi.DataAccessors
+{
+    public class RegisterDeviceAccessor : IRegisterDeviceAccessor
+    {
+        public Task RegisterNewDevice(RegisterDevice registerDevice)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
